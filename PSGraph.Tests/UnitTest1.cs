a826a58c382b08c05bc36d915b74688524ee124b
Nazgul <s.nazgul@gmail.com>
2016-02-03 00:33:18 +0200

    Create unit test project

---
+++ PSGraph.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PSGraph.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.Fail("fail test");
+		}
+	}
+}
