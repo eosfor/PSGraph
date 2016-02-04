fbd5db5a8b8c61b5f0816a7da5e0c87a16f94bee
Nazgul <s.nazgul@gmail.com>
2016-02-05 00:30:01 +0200

    "New-Graph" was implemented

---
+++ PSGraph.Tests/PSHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSGraph.Tests
+{
+	class PSHelpers
+	{
+		static public Object RunPSCmdlet(PSCmdlet cmdlet)
+		{
+            //PowerShell powershell = PowerShell.Create();
+            //powershell.AddCommand("Import-Module").AddParameter("Assembly", System.Reflection.Assembly.GetExecutingAssembly());
+
+            //Pipeline pipeLine = powershell.Runspace.CreatePipeline();
+
+            //PowerShell ps = PowerShell.Create();
+
+            //ps.Invoke();
+            //ps.Commands.Clear();
+            return null;
+		}
+	}
+}
