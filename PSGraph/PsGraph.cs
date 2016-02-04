9725c07a8e84ca3b44ac7fa5d766e133fc3f9c4d
Nazgul <s.nazgul@gmail.com>
2016-02-05 00:30:01 +0200

    "New-Graph" was implemented

--- PSGraph/PsGraph.cs
+++ PSGraph/PsGraph.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management.Automation;
+
 
 namespace PSGraph
 {
-    public class PsGraph
+	[Cmdlet(VerbsCommon.New, "Graph")]
+	public class PsGraph : PSCmdlet
 	{
-    }
+		private string _grapthType;
+
+		[Parameter(Mandatory = true, HelpMessage= "no one is supported")]
+		public string Type
+		{
+			get { return _grapthType; }
+			set { _grapthType = value; }
+		}
+
+		protected override void ProcessRecord()
+		{
+			WriteVerbose("New PsGraph: " + _grapthType);
+			//WriteObject("New PsGraph: " + _grapthType);
+		}
+	}
 }
