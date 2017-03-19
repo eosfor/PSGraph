adfb9ad7ca88c2c6745fbf07d6530fbd972111eb
eosfor <eosfor@gmail.com>
2017-03-19 22:15:37 +0300

    first test  SPF Dijkstra

---
+++ PSGraph/GetGraphPath.cs
@@ -0,0 +1,61 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using QuickGraph.Algorithms;
+
+namespace PSGraph
+{
+    [Cmdlet(VerbsCommon.Get, "GraphPath")]
+    public class GetGraphPath : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Object From { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public Object To { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public Object Graph { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Object graph = Graph;
+            if (graph is PSObject)
+                graph = ((PSObject)graph).ImmediateBaseObject;
+            if (graph == null)
+            {
+                throw new System.ArgumentException("'Graph' mustn't be equal to null");
+            }
+
+            Object from = From;
+            if (from is PSObject)
+                from = ((PSObject)from).ImmediateBaseObject;
+            if (from == null)
+            {
+                throw new System.ArgumentException("'From' mustn't be equal to null");
+            }
+
+            Object to = To;
+            if (to is PSObject)
+                to = ((PSObject)to).ImmediateBaseObject;
+            if (to == null)
+            {
+                throw new System.ArgumentException("'To' mustn't be equal to null");
+            }
+
+
+            Func<STaggedEdge<object, object>, double> edgeWeights = (j => { return 1; }); //all edges are equal
+            var tryGetFunc = ((AdjacencyGraph<object, STaggedEdge<object, object>>)graph).ShortestPathsDijkstra(edgeWeights, from);
+
+            IEnumerable<STaggedEdge<object, object>> path;
+            if (tryGetFunc(to, out path))
+            {
+                WriteObject(path);
+            }
+        }
+    }
+}
--- PSGraph/graphtest.ps1
+++ PSGraph/graphtest.ps1
@@ -11,13 +11,23 @@ class ER : Psgraph.PSGraphVertex {
     [string]$type = "ER"
 }
 
-Add-Edge -from ([vnet]@{Label = "aaa";  shape = "Parallelogram"; iprange = "192.168.1.0/24"}) -to ([ER]@{Label = "xxx";  ipaddress = "192.168.2.1"}) -Graph $g
-Add-Edge -from ([vnet]@{Label = "bbb";  iprange = "192.168.2.0/24"}) -to ([ER]@{Label = "yyy";  ipaddress = "192.168.3.1"}) -Graph $g
+[ER]$er  = @{Label = "yyy";  ipaddress = "192.168.3.1"}
+[ER]$erEnd  = @{Label = "end";  ipaddress = "192.168.10.1"}
+
+Add-Edge -from ([vnet]@{Label = "aaa";  shape = "Parallelogram"; iprange = "192.168.1.0/24"}) -to $er -Graph $g
+Add-Edge -from ([vnet]@{Label = "bbb";  iprange = "192.168.2.0/24"}) -to $er -Graph $g
+Add-Edge -from $er -to $erEnd -Graph $g
 Add-Edge -from ([vnet]@{Label = "ccc";  iprange = "192.168.2.0/24"}) -to ([ER]@{Label = "zzz";  ipaddress = "192.168.4.1"}) -Graph $g
 
 
+$e = $g.vertices | where iprange -EQ "192.168.1.0/24"
+$e1 = $g.vertices | where ipaddress -EQ "192.168.10.1"
+
+get-graphpath -from $e -to $e1 -graph $g
 
 Export-Graph -Graph $g -Format Graphviz -Path c:\tests\testgraph.txt
+
+
 
 
 C:\Temp\graphviz\release\bin\dot.exe -Tpng "C:\tests\testgraph.txt" -o 'C:\tests\testgraph.png'
