2cb34123629b077a350caf5fabb4831be2d127cc
Andrey Vernigora <eosfor@gmail.com>
2017-04-11 08:45:12 +0300

    few pester based tests added

---
+++ PSGraph.Tests/psgraph.pester.tests.ps1
@@ -0,0 +1,28 @@
+﻿describe "PSGraph Pester test" {
+	context "Create graph  test" {
+		it "Creating graph"  {
+			$g = New-Graph -Type AdjacencyGraph -EnableVertexComparer
+			$g | Should Not BeNullOrEmpty
+		}
+	}
+	context "Test for uniqueness" {
+		it "Test for uniqueness" {
+			class vnet : Psgraph.PSGraphVertex {
+				[string]$iprange;
+				[string]$type = "vnet"
+			}
+
+			class ER : Psgraph.PSGraphVertex {
+				[string]$ipaddress;
+				[string]$type = "ER"
+			}
+
+			[ER]$er  = @{Label = "yyy";  ipaddress = "192.168.3.1"}
+			[ER]$erEnd  = @{Label = "end";  ipaddress = "192.168.10.1"}
+
+			$g = New-Graph -Type AdjacencyGraph -EnableVertexComparer
+			Add-Edge -from ([vnet]@{Label = "aaa";  shape = "Parallelogram"; iprange = "192.168.1.0/24"}) -to $er -Graph $g | should be $true
+			Add-Edge -from ([vnet]@{Label = "aaa";  shape = "Parallelogram"; iprange = "192.168.1.0/24"}) -to $er -Graph $g | should be $false
+		}
+	}
+}
