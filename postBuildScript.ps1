3686685eb7f047b50cf06b097c5d0b1feb271200
Andrey Vernigora <andrei.vernigora@outlook.com>
2017-04-25 16:43:11 +0300

    small update
    1. docs updated
    2. some post build stuff scripted
    3. Module manifest updated

---
+++ postBuildScript.ps1
@@ -0,0 +1,12 @@
+﻿$destinationFolder = "C:\Users\Andrey_Vernigora\Documents\WindowsPowerShell\Modules\PSQuickGraph"
+$docsFolder = Get-Item "..\docs"
+
+
+dir $destinationFolder | Remove-Item -Force
+copy .\bin\Release\*.dll $destinationFolder
+copy .\bin\Release\*.pdb $destinationFolder
+copy .\bin\Release\PSQuickGraph.psd1 $destinationFolder
+
+Import-Module platyPS
+Import-Module PSQuickGraph
+New-ExternalHelp $docsFolder -OutputPath (Join-Path -Path $destinationFolder -Childpath 'en-US')
