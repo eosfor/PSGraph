02bea35e79e799f8411dc67575af1b76840b27e8
eosfor <eosfor@gmail.com>
2017-04-25 15:55:23 +0300

    export types moved back

---
+++ PSGraph/ExportTypes.cs
@@ -0,0 +1,32 @@
+﻿using QuickGraph.Graphviz.Dot;
+using System.Collections.Generic;
+
+namespace PSGraph
+{
+    public enum ExportTypes
+    {
+        Graphviz
+    }
+
+    public abstract class PSGraphVertex : GraphvizVertex
+    {
+        public override int GetHashCode()
+        {
+            return UniqueKey.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return UniqueKey == ((PSGraphVertex)obj).UniqueKey;
+        }
+
+        /// <summary>
+        /// Vertex unique key, eg Label + ResourceId
+        /// </summary>
+        public abstract string UniqueKey { get; }
+    }
+}
