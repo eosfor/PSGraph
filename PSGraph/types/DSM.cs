2d896398fb9bffbb47fb1019e948841fb2cee156
eosfor <eosfor@xxx.com>
2021-12-03 20:18:17 -0800

    + dsm class

---
+++ PSGraph/types/DSM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSGraph.DSM
+{
+    public class DSM
+    {
+        public int[,] matrix = null;
+        public DSM(int i, int j)
+        {
+            matrix = new int[i, j];
+        }
+
+        public void Cluster()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
