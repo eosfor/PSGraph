d720f3d1e4e8a331271b1939059a3eb4c96d5f97
eosfor <eosfor@xxx.com>
2021-12-04 16:21:25 -0800

    added a socond project; added limited import from dot files (through dotparse)

---
+++ PSGraphv2/cmdlets/ImportGraphCmdlet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using QuikGraph;
+using DotParser;
+using QuikGraph;
+
+namespace PSGraph
+{
+    [Cmdlet(VerbsData.Import, "Graph")]
+    public class ImportGraphCmdlet: PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string? Path;
+        protected override void ProcessRecord()
+        {
+            var s = File.ReadAllText(Path, Encoding.UTF8);
+            var p = DotParser.DotParser.parse(s);
+
+            var g = new BidirectionalGraph<object, Edge<object>>();
+
+            foreach (var node in p.Nodes)
+            {
+                g.AddVertex(node.Value["label"]);
+            }
+            
+
+            foreach (var e in p.Edges)
+            {
+                var from = p.Nodes.Where(n => n.Key == e.Key.Item1).First().Value["label"];
+                var to = p.Nodes.Where(n => n.Key == e.Key.Item2).First().Value["label"];
+                var newEdge = new Edge<object>(from, to);
+                g.AddEdge(newEdge);
+            }
+
+            WriteObject(g);
+
+            //base.ProcessRecord();
+        }
+    }
+}
