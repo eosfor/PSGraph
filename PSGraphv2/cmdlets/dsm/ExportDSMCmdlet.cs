849209165686e06dc383e206af8e97b912112ad1
eosfor <eosfor@xxx.com>
2021-12-04 23:53:37 -0800

    init. can now create a DMS from a bidirectional graph and export it to SVG

---
+++ PSGraphv2/cmdlets/dsm/ExportDSMCmdlet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using PSGraph.DSM;
+using Svg;
+
+namespace PSGraph.cmdlets.dsm
+{
+    [Cmdlet(VerbsData.Export, "DSM")]
+    public class ExportDSMCmdlet : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public DSM.dsm Dsm;
+
+        [Parameter(Mandatory = true)]
+        public string Path;
+        protected override void ProcessRecord()
+        {
+            var svgDoc = new SvgDocument()
+            {
+                Width = Dsm.Size * 10 + 200,
+                Height = Dsm.Size * 10 + 200,
+                ViewBox = new SvgViewBox() { MinX = 0, MinY = 0, Height = Dsm.Size * 10, Width = Dsm.Size * 10 }
+            };
+
+            var svgGroup = new SvgGroup();
+            svgDoc.Children.Add(svgGroup);
+
+            for (int i = 0; i <= Dsm.Size - 1; i++)
+            {
+                var row = i * 10 + 100;
+                for (int j = 0; j <= Dsm.Size - 1; j++)
+                {
+                    var col = j * 10 + 100;
+                    var rect = new SvgRectangle() { 
+                        Width = 10, 
+                        Height = 10, 
+                        X = row, 
+                        Y = col, 
+                        StrokeWidth = 1, 
+                        Stroke = new SvgColourServer(System.Drawing.Color.Black) 
+                    };
+                    if (Dsm[i, j] == 0)
+                    {
+                        rect.Fill = new SvgColourServer(System.Drawing.Color.White);
+                    }
+                    else
+                    {
+                        rect.Fill = new SvgColourServer(System.Drawing.Color.Red);
+                    }
+
+                    svgDoc.Children.Add(rect);
+
+                }
+            }
+
+            svgDoc.Write(Path);
+
+            //base.ProcessRecord();
+        }
+    }
+}
--- PSGraphv2/cmdlets/graph/ImportGraphCmdlet.cs
+++ PSGraphv2/cmdlets/graph/ImportGraphCmdlet.cs
@@ -20,7 +20,7 @@ namespace PSGraph
             var s = File.ReadAllText(Path, Encoding.UTF8);
             var p = DotParser.DotParser.parse(s);
 
-            var g = new BidirectionalGraph<object, Edge<object>>();
+            var g = new BidirectionalGraph<object, STaggedEdge<object, object>>();
 
             foreach (var node in p.Nodes)
             {
@@ -32,7 +32,7 @@ namespace PSGraph
             {
                 var from = p.Nodes.Where(n => n.Key == e.Key.Item1).First().Value["label"];
                 var to = p.Nodes.Where(n => n.Key == e.Key.Item2).First().Value["label"];
-                var newEdge = new Edge<object>(from, to);
+                var newEdge = new STaggedEdge<object, object>(from, to, null);
                 g.AddEdge(newEdge);
             }
 
