c3a6ca6cbb83e94723e169fa3914c60b4f0c5d96
eosfor <eosfor@gmail.com>
2022-10-05 18:16:11 -0700

    some sample scripts update

--- PSGraphv2/PSScripts/simpleGraphSample.ps1
+++ PSGraphv2/PSScripts/simpleGraphSample.ps1
@@ -53,37 +53,11 @@ Export-Graph -Graph $g -Format MSAGL_MDS -Path $svgOutFile
 # $svgOutFile
 
 $d = New-DSM -Graph $g
-Start-DSMClustering -Dsm $d
+$ret = Start-DSMClustering -Dsm $d
+
 Export-DSM -Dsm $d -Path $Env:TMPDIR/dsm.svg -Format SVG
 Export-DSM -Dsm $d -Path $Env:TMPDIR/dsm.txt -Format TEXT
 
-Export-DSM -Dsm $d.Power(2) -Path $Env:TMPDIR/dsm2.svg -Format SVG
-Export-DSM -Dsm $d.Power(2) -Path $Env:TMPDIR/dsm2.txt -Format TEXT
-
-Export-DSM -Dsm $d.Power(3) -Path $Env:TMPDIR/dsm3.svg -Format SVG
-Export-DSM -Dsm $d.Power(3) -Path $Env:TMPDIR/dsm3.txt -Format TEXT
-
-Export-DSM -Dsm $d.Power(4) -Path $Env:TMPDIR/dsm4.svg -Format SVG
-Export-DSM -Dsm $d.Power(4) -Path $Env:TMPDIR/dsm4.txt -Format TEXT
-
-Export-DSM -Dsm $d.Power(5) -Path $Env:TMPDIR/dsm5.svg -Format SVG
-Export-DSM -Dsm $d.Power(5) -Path $Env:TMPDIR/dsm5.txt -Format TEXT
-
-Export-DSM -Dsm $d.Power(6) -Path $Env:TMPDIR/dsm6.svg -Format SVG
-Export-DSM -Dsm $d.Power(6) -Path $Env:TMPDIR/dsm6.txt -Format TEXT
-
+Export-DSM -Result $ret -Path $Env:TMPDIR/dsmPartitioned.svg
 
 dot -Tsvg $graphFile -o $env:TMPDIR/srcGraph.svg
-
-open $env:TMPDIR/srcGraph.svg
-open $Env:TMPDIR/dsm.svg
-open $Env:TMPDIR/dsm.txt
-open $Env:TMPDIR/dsm3.svg
-open $Env:TMPDIR/dsm4.svg
-open $Env:TMPDIR/dsm5.svg
-open $Env:TMPDIR/dsm6.svg
-
-open $Env:TMPDIR/dsm3.txt
-open $Env:TMPDIR/dsm4.txt
-open $Env:TMPDIR/dsm5.txt
-open $Env:TMPDIR/dsm6.txt
